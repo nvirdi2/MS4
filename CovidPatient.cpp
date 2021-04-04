c0d7040114e154b215a39fbef313c27f8ff7cd06
nvirdi2 <71736420+nvirdi2@users.noreply.github.com>
2021-04-03 20:42:05 -0400

    Add files via upload

---
+++ CovidPatient.cpp
@@ -0,0 +1,54 @@
+// Name: Navdeep Virdi
+// Seneca email: nvirdi2@myseneca.ca
+// Student ID: 166485193
+// Date: April 3rd, 2021
+
+//I have done all the coding by myself and only copied the code that my professor provided to complete my workshops and assignments.
+
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <iostream>
+
+#include "CovidPatient.h"
+
+namespace sdds 
+{
+    char CovidPatient::type() const 
+    {
+        return 'C';
+    }
+
+    int C_Ticket = 1;
+
+    CovidPatient::CovidPatient() :Patient(C_Ticket) 
+    {
+        C_Ticket++;
+    }
+
+    std::istream& CovidPatient::read(std::istream& is) 
+    {
+        return fileIO() ? csvRead(is) : Patient::read(is);
+    }
+
+    std::istream& CovidPatient::csvRead(std::istream& is) 
+    {
+        Patient::csvRead(is);
+        C_Ticket = Patient::number() + 1;
+            return is;
+    }
+
+    std::ostream& CovidPatient::write(std::ostream& os) const 
+    {
+        if (!(fileIO())) 
+        {
+            os << "COVID TEST" << std::endl;
+            Patient::write(os);
+            os << std::endl;
+        }
+        
+        else 
+        {
+            csvWrite(os);
+        } return os;
+    }
+}
